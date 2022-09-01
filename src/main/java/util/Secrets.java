d2cd422a095d36d277db2e0af807cd47402ddfa6
Trenton <dr.teasdale.trenton@gmail.com>
2022-09-01 09:42:23 -0600

    deletes

--- src/main/java/util/Secrets.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class Secrets {
-	static final String url = "jdbc:postgresql://trentondb.c3i9uiji40un.us-west-1.rds.amazonaws.com:5432/postgres";
-	static final String user = "trentont";
-	static final String pass = "Teasdale360";
-}
